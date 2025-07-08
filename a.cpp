1604c1d89a1353fb534c592e33e3010c1cb6f69d
Denis396454 <wadimzzzz888@gmail.com>
2025-07-08 22:45:27 +0300

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+    int a=10;
+for ( int b = 1; b <= 10; b++){
+    cout << a << endl;
+}
+    system("color A");
+for ( int b = 1; b <= 10; b++){
+    cout << a << endl;
+}
+    system("color B");
+for ( int b = 1; b <= 10; b++){
+    cout << a << endl;
+}
+    return 0;
+}
